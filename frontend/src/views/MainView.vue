cb31aaf7dc23b7c3543b305837cc67f921327a28
fatimatuana <fatima__tuana@hotmail.com>
2025-05-31 23:59:09 +0200

    the main view is created (no props between the components for now)

---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <WeatherInfo />
+    <MoodComponent />
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import WeatherInfo from '@/components/WeatherInfoComponent.vue';
+import MoodComponent from '@/components/MoodComponent.vue';
+export default {
+  name: "MainView",
+  components: {
+    WeatherInfo,
+    MoodComponent,
+  },
+};
+</script>
+<style scoped>
+.main {
+  padding: 20px;
+}
+</style>
