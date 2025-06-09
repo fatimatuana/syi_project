26ec8831057c861903f6e9963ca0e32d99574948
fatimatuana <fatima__tuana@hotmail.com>
2025-06-09 22:57:07 +0200

    general design changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <!-- eslint-disable -->
+   <div>
+    <v-app-bar v-if="loggedin === true" app color="primary" dark id="nav-bar">
+      <div>
+        <a><router-link to="/" tag="button">My Daily Companion</router-link></a>
+        <a><router-link to="/history" tag="button">My History</router-link></a>
+        <a id="logout" tag="button" @click="logout">Logout</a>
+      </div>
+      </v-app-bar>
    <v-container fluid>
     <router-view />
    </v-container>
-
+</div>
 </template>
 <script>
 /* eslint-disable */
-
+export default {
+  name: "AppView",
+  components: {
+   
+  },
+  data() {
+    return {
+      loggedin: localStorage.getItem('loggedin'),
+    };
+  },
+  methods: {
+    logout() {
+      localStorage.setItem('loggedin', false);
+      this.$router.push('/login'); 
+    }
+  },
+  created() {
+    this.loggedin = localStorage.getItem('loggedin') === 'true';
+    if (!this.loggedin) {
+      this.$router.push('/login'); // redirect to login if not logged in
+    }
+  },
+};
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Rouge+Script&display=swap');
 *{
   font-family: 'Outfit', Avenir, Helvetica, Arial, sans-serif;
 }
@@ -23,7 +54,16 @@
 }
 
 body{
-  background-color: #0e0e0e;
+  /* background-color: #0e0e0e; */
+  background: #020024;
+/* background: radial-gradient(circle,rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%); */
+/* background: linear-gradient(90deg,rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%); */
+background: linear-gradient(to top right, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%,rgba(0, 212, 205, 1) 100%, rgba(0, 212, 255, 1) 100%);
+background-repeat: no-repeat;
+background-size: cover;
+  height: 100vh;
+  margin: 0;
+
 }
 
 p.--big, .--big{
@@ -35,4 +75,82 @@ p{
   font-size: 1em;
 }
 
+button{
+  border-radius: 10px;
+  padding: 10px 20px;
+  font-size: 1em;
+  border: none;
+  cursor: pointer;
+}
+
+.btn-primary{
+  /* background-color: #020024; */
+  background-color: #dadada;
+  color: rgba(9, 9, 121, 1);
+  box-shadow: 0px 4px 10px rgba(10, 10, 10, 0.3);
+}
+.btn-primary:disabled{
+  /* background-color: #020024; */
+  background-color: #1976d238;
+  color: #ffffff4f;
+  cursor: not-allowed;
+}
+.btn-secondary{
+  /* background-color: #020024; */
+  background-color: rgba(9, 9, 121, 1);
+  color:#dadada;
+  box-shadow: 0px 4px 10px rgba(10, 10, 10, 0.3);
+}
+button:disabled{
+  /* background-color: #020024; */
+  background-color: #1976d238;
+  color: #ffffff4f;
+  cursor: not-allowed;
+  box-shadow: 0px 0px;
+}
+.mt-1{
+  margin-top: 1vh;
+}
+.mt-3{
+  margin-top: 3vh;
+}
+.mt-5{
+  margin-top: 5vh;
+}
+.--detail{
+  font-size: 0.8em;
+  color: #fafafa;
+}
+a#logout{
+  color: rgb(9 9 121 / 84%);
+  text-decoration: none;
+  font-size: 1em;
+  margin-left: auto;
+}
+a:hover, button:hover{
+  cursor: pointer;
+}
+a.--detail{
+  text-decoration: underline;
+  color: #aeaeae;
+}
+.material-icons{
+  font-size: 34px;
+}
+.font--logo{
+  font-family: "Rouge Script", cursive;
+  font-weight: 400;
+  font-style: normal;
+}
+input {
+  width: 100%;
+  padding: 12px 15px;
+  margin: 0px 0px 12px 0px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+}
+#nav-bar{
+
+}
 </style>
