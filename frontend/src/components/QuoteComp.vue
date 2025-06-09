b4d6297af78f8b4bb392b1b2e5c94879251eb240
fatimatuana <fatima__tuana@hotmail.com>
2025-06-09 22:56:05 +0200

    the quote component is created successfully

---
+++ frontend/src/components/QuoteComp.vue
@@ -0,0 +1,51 @@
+<!-- eslint-disable -->
+<template>
+    <div class="quote-comp">
+        <p><i>"{{ quoteobj.quote }}"</i></p>
+        <p>— {{ quoteobj.author }}</p>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+import axios from "axios";
+export default {
+  name: "QuoteComp",
+  data() {
+    return {
+      quoteobj: {
+        quote: "",
+        author: ""
+      }
+    };
+  },
+  methods: {
+    async getQuote() {
+    //   let res = await axios.get("http://localhost:3000/quotes/today");
+    //   if(res.status !== 200) {
+    //     console.error("Failed to fetch quote:", res.statusText);
+    //     this.quoteobj = {
+    //       quote: "No quote available",
+    //       author: "Unknown"
+    //     };
+    //     return;
+    //   }
+    //   let data = res.data;
+    //   console.log("Quote data received:", data);
+    //   this.quoteobj = {
+    //     quote: data[0].quote,
+    //     author: data[0].author
+    //   };
+      this.quoteobj.quote ="One is never afraid of the unknown; one is afraid of the known coming to an end.";
+        this.quoteobj.author = "Jiddu Krishnamurti";
+    }
+  },
+  created() {
+    this.getQuote();
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
