45d0cae949854a02f786b6649ac838d2d6f0bc31
fatimatuana <fatima__tuana@hotmail.com>
2025-06-09 22:53:28 +0200

    updated the login component

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -5,7 +5,7 @@
       <h2>Login</h2>
       <input type="email" v-model="email" placeholder="Email" />
       <input type="password" v-model="password" placeholder="Password" />
-      <button @click="handleLogin">Login</button>
+      <button class="btn-secondary" @click="handleLogin">Login</button>
       <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
     </div>
   </div>
@@ -26,24 +26,29 @@ export default {
   methods: {
     async handleLogin() {
       this.errorMessage = "";
-      await axios.post("....", {
+
+      if (!this.email || !this.password) {
+        this.errorMessage = "Please enter both email and password.";
+        return;
+      }
+
+      try {
+        const response = await axios.post("....", {
           email: this.email,
           password: this.password
-      }).then(function (response) {
-          console.log(response);
-          // Handle successful login, e.g., redirect to main view
-          if (response.data.success) {
-              // Redirect to main view  // maybe to another view where the user can enter the location too
-              this.$router.push({ path: '/main' });
-          } else {
-              this.errorMessage = response.data.message || "Login failed. Please try again.";
-          }
-      })
-      .catch(function (error) {
-          this.errorMessage = "An error occurred. Please try again.";
-          console.error(error);
-      });
-    },
+        });
+
+        if (response.data.success) {
+          this.$router.push({ path: '/main' });
+          localStorage.setItem('loggedin', true); // hardcoded for now
+        } else {
+          this.errorMessage = response.data.message || "Login failed. Please try again.";
+        }
+      } catch (error) {
+        this.errorMessage = "An error occurred. Please try again.";
+        console.error(error);
+      }
+    }
   },
 };
 </script>
@@ -57,37 +62,21 @@ export default {
 }
 
 .login-box {
-  padding: 30px 40px;
+  padding: 30px 40px 60px;
   border-radius: 10px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   width: 300px;
   text-align: center;
-}
-
-.login-box input {
-  width: 100%;
-  padding: 10px;
-  margin: 0px 0px 12px 0px;
-  box-sizing: border-box;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  background-color: rgb(255 255 255 / 5%);
+  backdrop-filter: blur(10px);
 }
 
 .login-box button {
   width: 100%;
-  padding: 10px;
-  background-color: #303030;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  margin: 2px 0px;
 }
-
 .login-box button:hover {
   background-color: #1565c0;
 }
-
 .error {
   color: red;
   margin-top: 10px;
